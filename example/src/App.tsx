934d40ff86cd79edb95ab2e71d349caa721fadea
Simone Marinelli <simone.marinelli@cdt.ch>
2022-11-03 18:20:04 +0100

    refactor: update banner

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { SafeAreaView } from 'react-native';
-import { Banner } from 'react-native-banner';
+import { AppNexusBanner } from 'react-native-banner';
 
 export default function App() {
   return (
     <SafeAreaView>
-      <Banner placementId={'14460918'} sizes={[[300, 250]]} />
+      <AppNexusBanner placementId={'14460918'} sizes={[[300, 250]]} />
     </SafeAreaView>
   );
 }
